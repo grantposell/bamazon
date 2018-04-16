0addd0f784b48704b90490b52b5cfd2b7967f943
grantposell <grantposell@gmail.com>
2018-04-15 23:29:37 -0500

    Added database

---
+++ bamazon.js
@@ -0,0 +1,21 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "password",
+    database: "bamazon_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
+});
